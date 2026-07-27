aa74cbd6ab4c4aaf102e20b5bbc6524bb3d8ba9d
sakai_takuro <takuro.sakai@sega.com>
2026-07-27 20:47:39 +0900

    Modify linking CRT

--- LlamaWrapper/build.bat
+++ LlamaWrapper/build.bat
@@ -1,4 +1,5 @@
-cmake -B build -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX="install" -DCMAKE_MSVC_RUNTIME_LIBRARY="MultiThreaded$<$<CONFIG:Debug>:Debug>" .
+cmake -B build -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX="install"
+REM -DCMAKE_MSVC_RUNTIME_LIBRARY="MultiThreaded$<$<CONFIG:Debug>:Debug>" .
 cmake --build build --config Release
 cmake --install build --config Release
 xcopy /Y .\install\bin\*.* ..\LlamaPure\runtimes\win-x64\native
